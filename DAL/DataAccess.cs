74fd8d383aa8d3e715afc277aaae13bac4a1bf70
RonenZ <ronenser@gmail.com>
2013-09-30 19:41:04 +0300

    first Commit

---
+++ DAL/DataAccess.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storm
+{
+    public class DataAccess : IDisposable
+    {
+        #region Private Variables
+        protected string conStr;
+        protected string prName;
+        private DbConnection cn;
+        private DbCommand com;
+        private DbProviderFactory factory;
+        #endregion
+
+        /// <summary>
+        /// Basic DAL object to communicate with data source
+        /// </summary>
+        /// <param name="ConStrName">Data Source Connection String Name in Web.Config</param>
+        public DataAccess(string ConStrName)
+            : this(ConfigurationManager.ConnectionStrings[ConStrName].ConnectionString, ConfigurationManager.ConnectionStrings[ConStrName].ProviderName) { }
+
+        /// <summary>
+        /// Basic DAL object to communicate with data source
+        /// </summary>
+        /// <param name="ConnectionString">Data source connection string</param>
+        /// <param name="ProviderName">Data source provider name</param>
+        public DataAccess(string ConnectionString, string ProviderName)
+        {
+            this.conStr = ConnectionString;
+            this.prName = ProviderName;
+
+            factory = DbProviderFactories.GetFactory(prName);
+            cn = factory.CreateConnection();
+            com = factory.CreateCommand();
+            com.Connection = cn;
+            cn.ConnectionString = conStr;
+        }
+
+        /// <summary>
+        /// returns DbType according to object type..
+        /// </summary>
+        /// <param name="Obj">object to be checked..</param>
+        /// <returns></returns>
+        public DbType GetDbType(object Obj)
+        {
+            DbType dbt;
+
+            //figuring out what DbType to send...
+            if (Obj is int)
+                dbt = DbType.Int32;
+            else if (Obj is string)
+                dbt = DbType.String;
+            else if (Obj is DateTime)
+                dbt = DbType.DateTime;
+            else if (Obj is bool)
+                dbt = DbType.Boolean;
+            else if (Obj is double)
+                dbt = DbType.Double;
+            else if (Obj is float)
+                dbt = DbType.Decimal;
+            else
+                dbt = DbType.Object;
+
+            return dbt;
+        }
+
+        /// <summary>
+        /// reutrns a DbParameter with the sent properties
+        /// </summary>
+        /// <param name="Name">Parameter name</param>
+        /// <param name="Type">Db type</param>
+        /// <param name="Value">Value</param>
+        /// <returns></returns>
+        public DbParameter CreateParam(string Name, DbType Type, object Value)
+        {
+            DbParameter prm = factory.CreateParameter();
+            prm.DbType = Type;
+            prm.ParameterName = Name;
+            prm.Value = Value;
+
+            return prm;
+        }
+
+        /// <summary>
+        /// reutrns a DbParameter with the sent properties
+        /// </summary>
+        /// <param name="Name">Parameter name</param>
+        /// <param name="Value">Value</param>
+        /// <returns></returns>
+        public DbParameter CreateParam(string Name, object Value)
+        {
+            DbType dbt = GetDbType(Value);
+
+            return CreateParam(Name, dbt, Value);
+        }
+
+        /// <summary>
+        /// reutrns a set of DbParameters with the sent properties
+        /// </summary>
+        /// <param name="Name">Parameter name</param>
+        /// <param name="Value">Value</param>
+        /// <returns></returns>
+        public DbParameter[] CreateParams(IEnumerable<object> Values)
+        {
+            DbParameter[] resultArr = new DbParameter[Values.Count()];
+            DbType dbt;
+            int i = 0;
+
+            foreach (var val in Values)
+            {
+                dbt = GetDbType(val);
+                resultArr[i] = CreateParam(string.Format("@{0}", i++), dbt, val);
+            }
+
+            return resultArr;
+        }
+
+        /// <summary>
+        /// opening connection
+        /// </summary>
+        public void Open()
+        {
+            if (cn.State != ConnectionState.Open)
+                cn.Open();
+        }
+
+        /// <summary>
+        /// query db 
+        /// </summary>
+        /// <param name="commandText">Select Query/Command Text</param>
+        /// <param name="Params">DbParameters</param>
+        /// <returns></returns>
+        public DbDataReader ExecuteQuery(string commandText)
+        {
+            com.CommandText = commandText;
+            Open();
+
+            return com.ExecuteReader();
+        }
+
+        /// <summary>
+        /// query db with command text and dbparameters
+        /// </summary>
+        /// <param name="commandText">Select Query/Command Text</param>
+        /// <param name="Params">DbParameters</param>
+        /// <returns></returns>
+        public DbDataReader QueryWithParameters(string commandText, params DbParameter[] Params)
+        {
+            try
+            {
+                com.Parameters.AddRange(Params);
+
+                return ExecuteQuery(commandText);
+            }
+            finally
+            {
+                com.Parameters.Clear();
+            }
+        }
+
+        /// <summary>
+        /// execute insert/update/delete commands
+        /// </summary>
+        /// <param name="commandText"></param>
+        /// <param name="Params"></param>
+        /// <returns></returns>
+        public int ExecuteNonQuery(string commandText, params DbParameter[] Params)
+        {
+            try
+            {
+                com.Parameters.AddRange(Params);
+                com.CommandText = commandText;
+                return com.ExecuteNonQuery();
+            }
+            finally
+            {
+                com.Parameters.Clear();
+            }
+        }
+
+        /// <summary>
+        /// execute Stored Procedures
+        /// </summary>
+        /// <param name="spName"></param>
+        /// <param name="Params"></param>
+        /// <returns></returns>
+        public DbDataReader ExecuteSP(string spName, params DbParameter[] Params)
+        {
+            try
+            {
+                com.CommandType = CommandType.StoredProcedure;
+                return QueryWithParameters(spName, Params);
+            }
+            finally
+            {
+                com.CommandType = CommandType.Text;
+            }
+        }
+
+        /// <summary>
+        /// closing connection
+        /// </summary>
+        public void Close()
+        {
+            if (cn.State != ConnectionState.Closed)
+                cn.Close();
+        }
+
+        /// <summary>
+        /// closing Dbconnection and disposing of objects...
+        /// </summary>
+        public void Dispose()
+        {
+            Close();
+            
+            if(com != null)
+                com.Dispose();
+
+            if(cn != null)
+                cn.Dispose();
+        }
+
+        ///// <summary>
+        ///// distructor function...
+        ///// </summary>
+        //~DataAccess()
+        //{
+        //    this.Dispose();
+        //}
+    }
+}
---
+++ DAL/Mapper.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storm
+{
+    public class Mapper
+    {
+        private static int counter = 0;
+        /// <summary>
+        /// returns enumeration of T from DbDataReader
+        /// </summary>
+        /// <returns></returns>
+        public T Parse<T>(DbDataReader reader, TableMapper<T> table) where T : class, new()
+        {
+            if (!reader.HasRows)
+                return null;
+
+            counter = 0;
+            T entity = new T();
+            int ordinal = 0;
+            foreach (var field in table.FieldsMapping)
+            {
+                ordinal = reader.GetOrdinal(field.Value);
+
+                if (ordinal < 0)
+                    continue;
+
+                Type t = reader.GetFieldType(ordinal);
+
+                try
+                {
+                    if (t.Name.Equals("DBNull"))
+                        continue;
+                    field.Key.SetValue(entity, reader[field.Value]);
+                }
+                catch { }
+            }
+
+            return entity;
+        }
+
+        /// <summary>
+        /// returns enumeration of T from DbDataReader
+        /// </summary>
+        /// <returns></returns>
+        public T ParseSingle<T>(DbDataReader reader, TableMapper<T> table) where T : class, new()
+        {
+            counter = 0;
+            reader.Read();
+
+            return Parse(reader, table);
+        }
+
+        /// <summary>
+        /// returns enumeration of T from DbDataReader
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> ParseMultiple<T>(DbDataReader reader, TableMapper<T> table) where T : class, new()
+        {
+            List<T> entities = new List<T>();
+
+            while (reader.Read())           
+            {
+                T entity = this.Parse(reader, table);
+                
+                if (entity == null)
+                    continue;
+
+                entities.Add(entity);
+            }
+
+            return entities;
+        }
+
+        /// <summary>
+        /// returns attribute value, seeking attribute array for a specific name
+        /// </summary>
+        /// <param name="attList">Array of Custom Attributes</param>
+        /// <param name="attName">name to seek for in attributes</param>
+        /// <returns></returns>
+        private string GetAttributeValueByName(IEnumerable<CustomAttributeData> attList, string attName)
+        {
+            CustomAttributeData result = attList.Where(w => w.AttributeType.Name == attName).FirstOrDefault();
+
+            if (result == null || result.ConstructorArguments.Count == 0)
+                return string.Empty;
+
+            return result.ConstructorArguments[0].Value.ToString();
+        }
+
+
+        /// <summary>
+        /// returns Id attribute value (mapping Id name)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        private string GetIdentityName<T>()
+        {
+            string attValue = string.Empty;
+            IEnumerable<PropertyInfo> props = typeof(T).GetProperties();
+
+            //searching for key attribute
+            foreach (var p in props)
+            {
+                attValue = GetAttributeValueByName(p.CustomAttributes, "KeyAttribute");
+
+                if (!string.IsNullOrEmpty(attValue))
+                    return attValue;
+            }
+
+            string propName, containId = string.Empty;
+
+            //if no key attribute searching Id or first name containing Id
+            foreach (var p in props)
+            {
+                propName = p.Name.Trim().ToLower();
+
+                if (propName.Equals("id"))
+                    return p.Name;
+
+                if (string.IsNullOrEmpty(containId) && propName.Contains("id"))
+                    containId = propName;
+            }
+
+            return containId;
+        }
+
+        /// <summary>
+        /// returns table attribute value (mapping table name)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>table attribute value</returns>
+        private string GetTableName<T>()
+        {
+            IEnumerable<CustomAttributeData> custAtts = typeof(T).CustomAttributes;
+            string attValue = GetAttributeValueByName(custAtts, "TableAttribute");
+
+            if (string.IsNullOrEmpty(attValue))
+                return typeof(T).Name;
+
+            return attValue;
+        }
+
+        /// <summary>
+        /// returns columns names array
+        /// </summary>
+        /// <returns>string array of columns</returns>
+        private IDictionary<PropertyInfo, string> GetColumns<T>()
+        {
+            Dictionary<PropertyInfo, string> result = new Dictionary<PropertyInfo, string>();
+
+            //reviewing all properties in class
+            IEnumerable<PropertyInfo> props = typeof(T).GetProperties();
+
+            string attValue;
+
+            foreach (var p in props)
+            {
+                attValue = this.GetAttributeValueByName(p.CustomAttributes, "ColumnAttribute");
+
+                if (string.IsNullOrEmpty(attValue))
+                    result.Add(p, p.Name);
+                else
+                    result.Add(p, attValue);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// return an TableMapper object with class table mapping data
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public TableMapper<T> GetTableMapper<T>()
+        {
+            TableMapper<T> result = new TableMapper<T>();
+            result.Table = this.GetTableName<T>();
+            result.Id = this.GetIdentityName<T>();
+            result.FieldsMapping = this.GetColumns<T>();
+            result.Columns = result.FieldsMapping.Select(s => s.Value).ToArray();
+
+            return result;
+        }
+    }
+}
